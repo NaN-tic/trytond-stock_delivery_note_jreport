51213a9c3e656c42fb03ff5b773d582f502d8878
Bernat Brunet Torruella <bernat@nan-tic.com>
2016-01-14 21:26:17 +0100

    Add config decimals use and refact part of report

--- __init__.py
+++ __init__.py
@@ -2,10 +2,15 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from .configuration import *
 from .shipment import *
 
 
 def register():
+    Pool.register(
+        Configuration,
+        StockConfigurationCompany,
+        module='stock_delivery_note_jreport', type_='model'),
     Pool.register(
         DeliveryNote,
         module='stock_delivery_note_jreport', type_='report')
