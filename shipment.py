5e2c813649a307ba56bbd9c2d3ea1c41dab35fa0
Raimon Esteve <raimon@nan-tic.com>
2017-03-14 19:23:36 +0100

    FIX type inherit form + PoolMeta

--- shipment.py
+++ shipment.py
@@ -1,11 +1,10 @@
-# This file is part jasper_reports module for Tryton.
+# This file is part stock_delivery_note_jreport module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import Pool
 from trytond.modules.jasper_reports.jasper import JasperReport
 
 __all__ = ['DeliveryNote']
-__metaclass__ = PoolMeta
 
 
 class DeliveryNote(JasperReport):
