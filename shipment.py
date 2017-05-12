9bf93e39ab7bdfa8f8158626201c4b337e6353ec
Raimon Esteve <raimon@nan-tic.com>
2017-05-12 14:09:36 +0200

    Overwrite delivery note report from stock module

--- shipment.py
+++ shipment.py
@@ -2,18 +2,21 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from trytond.pool import PoolMeta
 from trytond.modules.jasper_reports.jasper import JasperReport
 
 __all__ = ['DeliveryNote']
 
 
 class DeliveryNote(JasperReport):
-    __name__ = 'stock.shipment.out.delivery_note.jreport'
+    __metaclass__ = PoolMeta
+    __name__ = 'stock.shipment.out.delivery_note'
 
     @classmethod
     def execute(cls, ids, data):
         pool = Pool()
         Config = pool.get('stock.configuration')
+
         config = Config(1)
         parameters = {
             'shipment_qty_decimal': config.shipment_qty_decimal or False
